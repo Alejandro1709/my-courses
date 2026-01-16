4a854c1721e2f1c4db1366cfd1eeed08a64ddc54
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-01-16 13:56:59 -0500

    Add: Shadcn and Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import Header from "@/components/common/Header";
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="flex h-screen bg-background">
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Header />
+
+        <main className="flex-1 overflow-auto p-6 space-y-6">
+          <h1>Hello World</h1>
+        </main>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,46 @@
+import { Bell, Plus, Settings, User } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+function Header() {
+  return (
+    <header className="border-b border-border bg-card">
+      <div className="px-6 py-4 flex items-center justify-between">
+        <div className="">
+          <h1 className="text-2xl font-bold text-foreground">Mi ISIL</h1>
+          <p className="text-sm text-muted-foreground">Semestre 2026-0</p>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <Button className="gap-2 bg-primary hover:bg-primary/90">
+            <Plus className="w-4 h-4" />
+            Añadir Curso
+          </Button>
+
+          <Button
+            variant="ghost"
+            size="icon"
+            className="text-muted-foreground hover:text-foreground"
+          >
+            <Bell className="w-5 h-5" />
+          </Button>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="text-muted-foreground hover:text-foreground"
+          >
+            <Settings className="w-5 h-5" />
+          </Button>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="text-muted-foreground hover:text-foreground"
+          >
+            <User className="w-5 h-5" />
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
